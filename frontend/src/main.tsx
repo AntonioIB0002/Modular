65669f280dd8cc99f828c633a1bbcfa8028335e7
Antonio <126025312+AntonioIB0002@users.noreply.github.com>
2024-08-15 17:39:40 -0600

    add backend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,9 +6,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <SquidContextProvider
     options={{
-      appId: '44vgaem7tpwqndu1yw',
+      appId: 'xt01fcyhd56kellr99',
       region: 'us-east-1.aws', // example: 'us-east-1.aws'
-      environmentId: 'prod', // choose one of 'dev' or 'prod'
+      environmentId: 'dev', // choose one of 'dev' or 'prod'
       squidDeveloperId: 'pvtmwtlsa49ao8py3a',
     }}
   >
