c4186b872c71aa83f94b9a74463eb8a25667cf92
Antonio <126025312+AntonioIB0002@users.noreply.github.com>
2024-08-15 16:02:03 -0600

    ref front

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { SquidContextProvider } from '@squidcloud/react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <SquidContextProvider
+    options={{
+      appId: '44vgaem7tpwqndu1yw',
+      region: 'us-east-1.aws', // example: 'us-east-1.aws'
+      environmentId: 'prod', // choose one of 'dev' or 'prod'
+      squidDeveloperId: 'pvtmwtlsa49ao8py3a',
+    }}
+  >
     <App />
-  </StrictMode>,
-)
+  </SquidContextProvider>
+);
