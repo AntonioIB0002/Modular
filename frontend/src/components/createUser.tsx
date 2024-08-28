0eebb1a23d7147227f79f4ba8b2ca3075c758e71
Antonio <126025312+AntonioIB0002@users.noreply.github.com>
2024-08-28 12:47:27 -0600

    update test, createuser

--- frontend/src/components/createUser.tsx
+++ frontend/src/components/createUser.tsx
@@ -4,16 +4,23 @@ import { useCollection } from '@squidcloud/react';
 type User = { id: string; email: string; age: number };
 
 export default function CreateUser() {
-  const userCollection = useCollection<User>('users','modulardb_id');
+  const userCollection = useCollection<User>('users', 'modulardb_id');
+  
   const insert = () => {
     const userId = crypto.randomUUID();
     const email = `${userId}@email.com`;
+    
     userCollection.doc(userId).insert({
       id: userId,
       email,
       age: Math.ceil(Math.random() * 100),
+    }).then(() => {
+      console.log('User inserted successfully');
+    }).catch(error => {
+      console.error('Error inserting user:', error);
     });
   };
+  
   return (
     <>
       <button onClick={insert}>Empiece la prueba aqui</button>
--- test.py
+++ test.py
@@ -1,14 +1,3 @@
-# from sqlalchemy import create_engine, text
-
-
-# DATABASE_URL = "postgresql://us6aywspjna2d7hootm9:dssa41xvbdHEOwrc6TML@bwtfeqqk9qwynx1mgqxi-postgresql.services.clever-cloud.com:7195/bwtfeqqk9qwynx1mgqxi"
-# engine = create_engine(DATABASE_URL)
-
-# with engine.connect() as connection:
-#     # Convertir la cadena SQL en un objeto text
-#     result = connection.execute(text("SELECT version();"))
-#     for row in result:
-#         print(row)
 import psycopg2
 from psycopg2 import sql
 
@@ -45,15 +34,3 @@ finally:
     if conn:
         cursor.close()
         conn.close()
-
-# from pymongo.mongo_client import MongoClient
-# from pymongo.server_api import ServerApi
-# uri = "mongodb+srv://antonio:admin@cluster0.mzp1p9z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-# # Create a new client and connect to the server
-# client = MongoClient(uri, server_api=ServerApi('1'))
-# # Send a ping to confirm a successful connection
-# try:
-#     client.admin.command('ping')
-#     print("Pinged your deployment. You successfully connected to MongoDB!")
-# except Exception as e:
-#     print(e)
