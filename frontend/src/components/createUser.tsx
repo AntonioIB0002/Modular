3da9816e156387058944f0e1c8824581d5b4d76f
Antonio <126025312+AntonioIB0002@users.noreply.github.com>
2024-08-28 14:39:50 -0600

    Update createUser.tsx

--- frontend/src/components/createUser.tsx
+++ frontend/src/components/createUser.tsx
@@ -1,29 +1,48 @@
 import { useCollection } from '@squidcloud/react';
-
+import { useState } from 'react';
 // Define your type
-type User = { id: string; email: string; age: number };
+// type User = { id: string; email: string; age: number };
+type comentarios = {comentario:string}
 
 export default function CreateUser() {
-  const userCollection = useCollection<User>('users', 'modulardb_id');
-  
+  const [text, setText] = useState<string>('');
+
+  const userCollection = useCollection<comentarios>('comentarios','modulardb_id')
   const insert = () => {
-    const userId = crypto.randomUUID();
-    const email = `${userId}@email.com`;
-    
-    userCollection.doc(userId).insert({
-      id: userId,
-      email,
-      age: Math.ceil(Math.random() * 100),
-    }).then(() => {
-      console.log('User inserted successfully');
-    }).catch(error => {
-      console.error('Error inserting user:', error);
+    userCollection.doc().insert({
+      comentario:text,
+
     });
   };
+
+  // const userCollection = useCollection<User>('users', 'modulardb_id');
+  
+  // const insert = () => {
+  //   const userId = crypto.randomUUID();
+  //   const email = `${userId}@email.com`;
+    
+  //   userCollection.doc(userId).insert({
+  //     id: userId,
+  //     email,
+  //     age: Math.ceil(Math.random() * 100),
+  //   }).then(() => {
+  //     console.log('User inserted successfully');
+  //   }).catch(error => {
+  //     console.error('Error inserting user:', error);
+  //   });
+  // };
   
   return (
     <>
-      <button onClick={insert}>Empiece la prueba aqui</button>
+    <div>
+        <input 
+          type="text" 
+          value={text} 
+          onChange={(e) => setText(e.target.value)} 
+          placeholder="Ingresa el texto"
+        />
+        <button onClick={insert}>Analizar</button>
+    </div>
     </>
   );
 }
